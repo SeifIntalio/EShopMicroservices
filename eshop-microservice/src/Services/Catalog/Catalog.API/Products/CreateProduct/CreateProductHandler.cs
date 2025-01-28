bf6060e92cf72b3a1a03c047b15bcbdabcd3206d
Seif Seoud <seif.seoud@intalio.com>
2025-01-28 12:06:22 +0200

    add get products endpoints

--- eshop-microservice/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservice/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,27 @@
-﻿using MediatR;
-
+﻿
 namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductCommand(string Name , List<string> Category , string Description,string ImageFile , decimal Price)
-    :IRequest<CreateProductResult>;
+    :ICommand<CreateProductResult>;
 public record CreateProductResult(Guid Id);
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session) : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        // create product entity from command object
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // save the object to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
+        // return the createProductResult result
+        return  new CreateProductResult(product.Id);
     }
 }
